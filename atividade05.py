b909d846d2dc6bc3b7f12865aefc953814def530
hiarleyCsilva <hiarley.silva2@aluno.ce.gov.br>
2023-10-11 11:00:24 -0300

    conversao metros para cm e mm

---
+++ atividade05.py
@@ -0,0 +1,6 @@
+# Digite uma metragem e veja isso em cm e mm:
+metro1= float (input("'digite em metros:"))
+cm = (metro1 * 100)
+print ("sao exatamente:",cm,"cm")
+mm = (metro1 * 1000)
+print ("sao exatamente:",mm,"mm")
